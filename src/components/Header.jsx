68194cd055826ba7b9c53ecc715b2956789789b9
Amarnath Banerjee <amarnath.kbanerjee@gmail.com>
2025-01-03 12:35:58 +0530

    Implemented universal navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,53 @@
-import React from "react";
-import headerLogo from "../assets/logo.png";
+import React, { useState } from "react";
+import headerLogo from "../assets/Logos/logo.png";
+import navBtn from "../assets/Icons/menu.png";
+import $ from "jquery";
+import { use } from "react";
+
 function Header() {
+  const [blockClass, setBlockClass] = useState("block");
+  const [menuClass, setMenuClass] = useState("menu-list");
+
+  function clickFunction() {
+    if (blockClass === "block") {
+      setBlockClass("block-active");
+      setMenuClass("menu-list-active");
+    } else {
+      setBlockClass("block");
+      setMenuClass("menu-list");
+    }
+  }
+
   return (
     <header>
-      <div className="logo-container">
-        <img src={headerLogo} alt="Logo" />
+      <div className="header-show">
+        <div className="logo-container">
+          <img src={headerLogo} alt="Logo" />
+        </div>
+        <div className="menu-btn" onClick={clickFunction}>
+          <img src={navBtn} alt="Menu" className="nav-btn" />
+        </div>
       </div>
-      <div className="nav-container">
-        <a href="#">Pricing</a>
-        <a href="#">Work</a>
-        <a href="#">About</a>
+
+      <div className="header-menu">
+        <div className="menu-overlay">
+          <div className={`b1 ${blockClass}`}></div>
+          <div className={`b2 ${blockClass}`}></div>
+          <div className={`b3 ${blockClass}`}></div>
+          <div className={`b4 ${blockClass}`}></div>
+        </div>
+        <div className="strips">
+          <div className="strip one"></div>
+          <div className="strip two"></div>
+          <div className="strip three"></div>
+        </div>
+        <div className="menu">
+          <ul className={menuClass}>
+            <li className="menu-list-items">Pricing</li>
+            <li className="menu-list-items">Work</li>
+            <li className="menu-list-items">About</li>
+          </ul>
+        </div>
       </div>
     </header>
   );
