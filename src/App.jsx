68a220ea9686e577745fbc040dc98ec5eb196df5
Amarnath Banerjee <amarnath.kbanerjee@gmail.com>
2025-01-04 11:53:42 +0530

    added new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Experience from "./components/Experience";
 import Textblock from "./components/Textblock";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const slides = [
+    {
+      url: "/src/assets/Images/Carousel/carousel1.webp",
+      title: "Lorem",
+    },
+    {
+      url: "/src/assets/Images/Carousel/carousel2.webp",
+      title: "Ipsum",
+    },
+    {
+      url: "/src/assets/Images/Carousel/carousel3.webp",
+      title: "Dolor",
+    },
+    {
+      url: "/src/assets/Images/Carousel/carousel4.webp",
+      title: "Sit",
+    },
+    {
+      url: "/src/assets/Images/Carousel/carousel5.webp",
+      title: "Amet",
+    },
+  ];
   return (
     <>
       <Header></Header>
-      <Hero></Hero>
+      <Hero
+        heroIndex={0}
+        p1="Studio"
+        h="Flame Lustres"
+        p2="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam odit aliquid illum laboriosam commodi esse asperiores quibusdam iusto perferendis tempora modi iure molestias quas vel, eius quis eligendi, cupiditate consequuntur."
+        bi="image.png"
+      ></Hero>
+      <Experience
+        p1="Our"
+        h="Experience"
+        p2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quas recusandae dolorem facere? Atque natus minima eius aperiam quo voluptatibus suscipit maiores quasi. Necessitatibus saepe delectus veniam, nesciunt deserunt hic et, iste nemo consequatur nostrum sit quaerat explicabo odit aspernatur ab quo magni adipisci tempora amet quia dolorum voluptate. Et."
+        bi="image2.jpg"
+      ></Experience>
+      <Textblock></Textblock>
+      <Textblock></Textblock>
+      <Textblock></Textblock>
+      <Textblock></Textblock>
+      <Textblock></Textblock>
       <Textblock></Textblock>
     </>
   );
 }
-
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 // import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
-function Hero() {
+function Hero(props) {
   const [fontSize, setFontSize] = useState(3);
+  const [opac, setOpac] = useState(0);
+  const [z, setZ] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       // Calculate new font size based on scroll position
-      const newFontSize = 3 - window.scrollY / 40;
+      let newFontSize;
+      if (window.scrollY < window.innerHeight * props.heroIndex) {
+        newFontSize = 3;
+      } else {
+        newFontSize = Math.max(
+          1,
+          3 - (window.scrollY - window.innerHeight * props.heroIndex) / 80
+        );
+      }
+      const newOpac = window.scrollY / window.innerHeight + 0.5;
+      const newZ = Math.round(window.scrollY / window.innerHeight + 0.2);
 
       // Update font size state
       setFontSize(newFontSize);
+      setOpac(newOpac);
+      setZ(newZ);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -21,9 +35,15 @@ function Hero() {
   }, []);
 
   return (
-    <div className="bg-im">
-      <h1 style={{ fontSize: `${fontSize}rem` }}>Flame Lustress</h1>
-      <p>Studio</p>
+    <div
+      className="herobox"
+      style={{ backgroundImage: `url("/src/assets/Images/${props.bi}")` }}
+    >
+      <p>{props.p1}</p>
+      <h1 style={{ fontSize: `${fontSize}rem`, zIndex: `${z}` }}>{props.h}</h1>
+      <p className="hero-intro" style={{ opacity: `${opac}` }}>
+        {props.p2}
+      </p>
     </div>
   );
 }
